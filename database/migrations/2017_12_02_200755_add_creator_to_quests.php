2352ba4e3a983607856202e6c05b32981d4013de
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2017-12-02 21:10:20 +0100

    Added creator_id to quests

---
+++ database/migrations/2017_12_02_200755_add_creator_to_quests.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatorToQuests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quests', function (Blueprint $table) {
+            $table->unsignedInteger('creator_id')->index();
+            $table->foreign('creator_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quests', function (Blueprint $table) {
+            $table->dropColumn('creator_id');
+        });
+    }
+}
